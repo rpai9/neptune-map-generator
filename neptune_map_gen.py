e96325010cfc82d4b1ba31fbba72fcf7de02fc64
rpai9 <rohithr9@uw.edu>
2025-02-26 19:43:22 -0500

    Updated the code to add players

--- neptune_map_gen.py
+++ neptune_map_gen.py
@@ -1,11 +1,13 @@
-import numpy as np
-import random
+import configparser
 import json
 import os
-import configparser
-from names_generator import generate_name
+import math
+import random
 from datetime import datetime
+
+import numpy as np
 import plotly.graph_objects as go
+from names_generator import generate_name
 
 # Load configuration from file
 config = configparser.ConfigParser()
@@ -19,6 +21,47 @@ min_distance = config.getfloat("galaxy", "min_distance")
 radius_limit = config.getfloat("galaxy", "radius_limit")
 galaxy_type = config.get("galaxy", "galaxy_type")
 wormholes = json.loads(config.get("wormholes", "pairs"))
+num_player = config.getint("player_positioning", "num_players")
+max_dist_to_neighbor = config.getfloat("player_positioning", "max_dist_to_neighbor")
+ini_r = config.getint("player_initial_resource", "r")
+ini_g = config.getint("player_initial_resource", "g")
+ini_e = config.getint("player_initial_resource", "e")
+ini_i = config.getint("player_initial_resource", "i")
+ini_s = config.getint("player_initial_resource", "s")
+ini_st = config.getint("player_initial_resource", "st")
+
+
+def add_players_to_coordinates(coordinates):
+    player_indices = np.random.choice(len(coordinates), num_player, replace=False)
+    optimized_dist_min = radius_limit / (2 * math.sqrt(num_player))
+    optimized_dist_max = radius_limit / (2 * math.sqrt(math.log(num_player)))
+
+    if max_dist_to_neighbor < optimized_dist_min:
+        print("max_dist_to_neighbor is too small, players will be too close to each other")
+
+    if max_dist_to_neighbor > optimized_dist_max:
+        print("max_dist_to_neighbor is too large, players will be too far from each other")
+
+    optimized_dist = max_dist_to_neighbor
+    if max_dist_to_neighbor < optimized_dist_min or max_dist_to_neighbor > optimized_dist_max:
+        print("max_dist_to_neighbor is not in the optimal range, using the average value")
+        optimized_dist = (optimized_dist_min + optimized_dist_max) / 2
+
+    for i in range(1, num_player):
+        while True:
+            distances = np.linalg.norm(
+                np.array([coordinates[player_indices[i]]["x"], coordinates[player_indices[i]]["y"]])
+                - np.array([[coordinates[j]["x"], coordinates[j]["y"]] for j in player_indices[:i]]),
+                axis=1,
+            )
+            if np.all(distances > optimized_dist):
+                break
+            player_indices[i] = np.random.choice(len(coordinates))
+
+    for i, idx in enumerate(player_indices):
+        coordinates[idx].update({"puid": i + 1, "r": ini_r, "g": ini_g, "e": ini_e, "i": ini_i, "s": ini_s, "st": ini_st})
+
+    return coordinates
 
 
 def generate_spiral_galaxy(total_points, num_arms, pitch_angle, min_distance, radius_limit):
@@ -114,6 +157,9 @@ if __name__ == "__main__":
         stars.append(star)
         star_names.append(star_name)
 
+    # Add players to the coordinates
+    stars = add_players_to_coordinates(stars)
+
     result = {"stars": stars, "wormholes": wormholes}
 
     # Convert to JSON
@@ -130,7 +176,7 @@ if __name__ == "__main__":
         f.write(json_output)
 
     # Ensure coordinates is a 2D array
-    coordinates = np.array(coordinates)
+    coordinates = np.array([[star["x"], star["y"]] for star in stars])
     if coordinates.ndim != 2 or coordinates.shape[1] != 2:
         raise ValueError("Coordinates array is not in the correct shape")
 
@@ -138,17 +184,19 @@ if __name__ == "__main__":
     fig = go.Figure()
 
     # Add stars to the plot with tooltips
-    fig.add_trace(
-        go.Scatter(
-            x=coordinates[:, 0],
-            y=coordinates[:, 1],
-            mode="markers",
-            marker=dict(color="white", size=5, symbol="star"),
-            text=star_names,
-            hoverinfo="text",
-            name="Stars",
+    for star in stars:
+        color = "white" if "puid" not in star else f"rgb({random.randint(0, 255)}, {random.randint(0, 255)}, {random.randint(0, 255)})"
+        fig.add_trace(
+            go.Scatter(
+                x=[star["x"]],
+                y=[star["y"]],
+                mode="markers",
+                marker=dict(color=color, size=5, symbol="star"),
+                text=f"Index: {star['uid']} , Name: {star['name']} , Resources: {star['r']} , G: {star['g']} , E: {star['e']} , I: {star['i']} , S: {star['s']} ,ST: {star['st']}",
+                hoverinfo="text",
+                name="Stars" if "puid" not in star else f"Player {star['puid']}",
+            )
         )
-    )
 
     # Add wormholes to the plot
     for pair in wormholes:
@@ -169,11 +217,20 @@ if __name__ == "__main__":
         title={"text": f"Galaxy Map - {galaxy_type.capitalize()} Galaxy: EPOCH - {epoch}", "x": 0.5, "xanchor": "center"},  # Center the title
         xaxis_title="X Coordinate",
         yaxis_title="Y Coordinate",
-        plot_bgcolor="black",
+        plot_bgcolor="#202735",  # Hex color for black background
         showlegend=True,
         xaxis=dict(showgrid=False, zeroline=False),  # Remove x-axis grid and zero lines
         yaxis=dict(showgrid=False, zeroline=False),  # Remove y-axis grid and zero lines
     )
+
+    # Update traces to show only player stars and wormholes in the legend
+    for trace in fig.data:
+        if "Player" in trace.name:
+            trace.update(marker=dict(size=10, color=f"rgb({random.randint(0, 255)}, {random.randint(0, 255)}, {random.randint(0, 255)})"), showlegend=True)
+        elif trace.name == "Wormholes":
+            trace.update(showlegend=True)
+        else:
+            trace.update(showlegend=False)
 
     # Save the plot as an HTML file
     fig.write_html(f"{output_dir}/galaxy_map.html")
